22c785d0388fea7919cca098f61e64de924d4732
Lê Nguyễn Phú Hiệp <lenguyenphuhiep2000@gmail.com>
2023-09-26 14:55:04 +0700

    Kontent Filerobot

---
+++ main.js
@@ -0,0 +1,200 @@
+let kontentStoredAssets = [];
+const minHeight = 100;
+let token = false;
+let secTemplate = false;
+let rootPath = '/';
+let countFiles = 0;
+
+function updateDisabled(disabled) {
+    if (disabled) {
+        document.getElementById('filerobotOpenModalBtn').classList.add('hidden')
+    }
+    else {
+        document.getElementById('filerobotOpenModalBtn').classList.remove('hidden')
+    }
+}
+
+function buildHeight(files) {
+    const filesCount = files.length;
+    if (filesCount > 0) {
+        const rows = Math.ceil(filesCount / 6);
+        const newHeight = (250 * rows);
+        CustomElement.setHeight(newHeight);
+    } else {
+        CustomElement.setHeight(minHeight);
+    }
+}
+
+function buildHtml(selected) {
+    const assetUrl = selected.file.url.cdn;
+    const divElement = document.createElement('div');
+    divElement.classList = "rounded p-2 border shadow relative removeItemParent";
+    divElement.setAttribute("data-uuid", selected.file.uuid);
+
+    const imgElement = document.createElement('img');
+    imgElement.src = assetUrl + '&width=200&height=200';
+    imgElement.classList = "w-32 h-32 mx-auto rounded";
+    divElement.appendChild(imgElement);
+
+    const removeElement = document.createElement('div');
+    removeElement.classList = "absolute right-0 top-0 cursor-pointer";
+
+    const removeIconElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    removeIconElement.innerHTML = `
+                  <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd" />
+                `;
+    removeIconElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    removeIconElement.setAttribute("fill", "currentColor");
+    removeIconElement.classList = "w-6 h-6 text-red-500 hover:text-red-900 removeItem";
+
+    removeElement.appendChild(removeIconElement);
+    divElement.appendChild(removeElement)
+
+    document.getElementById('filerobotImages').appendChild(divElement);
+}
+
+function loadError() {
+    document.getElementById('message').innerText = "Please provide a token and secure template";
+    document.getElementById('messageWrapper').classList.remove('hidden');
+    document.getElementById('filerobotOpenModalBtn').classList.add('hidden');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    function loadFilerobot() {
+        const Filerobot = window.Filerobot;
+
+        if (token && secTemplate && rootPath) {
+            const filerobot = Filerobot.Core({
+                securityTemplateID: secTemplate,
+                container: token,
+                dev: false
+            });
+
+            // Plugins
+            const Explorer = Filerobot.Explorer;
+            const XHRUpload = Filerobot.XHRUpload;
+
+            filerobot
+                .use(Explorer, {
+                    config: {
+                        rootFolderPath: rootPath
+                    },
+                    target: '#filerobotWidget',
+                    inline: true,
+                    width: '99%',
+                    height: '500px',
+                    disableExportButton: false,
+                    hideExportButtonIcon: true,
+                    preventExportDefaultBehavior: true,
+                    resetAfterClose: true,
+                    dismissUrlPathQueryUpdate: true,
+                     // reference https://github.com/scaleflex/commercetools-filerobot-plugin/blob/master/src/components/filerobot/filerobot-dam.jsx
+                     locale: {
+                        strings: {
+                            mutualizedExportButtonLabel: 'Insert',
+                            mutualizedDownloadButton: 'Insert',
+                        }
+                    },
+                })
+                .use(XHRUpload)
+                .on('export', function (files, popupExportSuccessMsgFn, downloadFilesPackagedFn, downloadFileFn) {
+                    buildHeight(files)
+                    files.forEach((selected, key) => {
+                        const index = kontentStoredAssets.findIndex(item => item.file.uuid === selected.file.uuid);
+
+                        if (index === -1) {
+                            const storeData = {
+                                file: {
+                                    uuid: selected.file.uuid,
+                                    url: {
+                                        cdn: selected.file.url.cdn
+                                    }
+                                }
+                            }
+                            
+                            kontentStoredAssets.push(storeData);
+                            buildHtml(selected);
+                        }
+                    });
+
+                    if (files.length > 0) {
+                        document.getElementById('filerobotOpenModalBtn').classList.add('hidden')
+                        countFiles = files.length
+                    }
+                    closeModal();
+                    CustomElement.setValue(JSON.stringify(kontentStoredAssets));
+                });
+        }
+    }
+
+    CustomElement.init((element, _context) => {
+        const value = element.value;
+        const files = JSON.parse(value);
+        const config = element.config;
+
+        if (config) {
+            token = config.token ? config.token : false;
+            secTemplate = config.secTemplate ? config.secTemplate : false;
+            rootPath = config.rootPath ? config.rootPath : rootPath;
+
+            if (token && secTemplate) {
+                if (files) {
+                    kontentStoredAssets = files;
+                } else {
+                    kontentStoredAssets = [];
+                }
+
+                if (kontentStoredAssets.length > 0) {
+                    document.getElementById('filerobotOpenModalBtn').classList.add('hidden')
+                    files.forEach(function (selected, key) {
+                        buildHtml(selected)
+                    })
+                    countFiles = files.length
+                    buildHeight(files)
+                } else {
+                    CustomElement.setHeight(minHeight);
+                }
+                CustomElement.onDisabledChanged(updateDisabled);
+                loadFilerobot();
+                return;
+            }
+        }
+        loadError();
+    });
+
+    function closeModal(newHeight) {
+        document.getElementById('filerobotWidgetWrapper').classList.add('hidden');
+    }
+
+    const openModalBtn = document.getElementById('filerobotOpenModalBtn');
+    const closeModalBtn = document.getElementById('filerobotCloseModalBtn');
+    openModalBtn.addEventListener('click', function () {
+        CustomElement.setHeight(650);
+        document.getElementById('filerobotWidgetWrapper').classList.remove('hidden');
+    })
+
+    closeModalBtn.addEventListener('click', function () {
+        closeModal();
+        buildHeight(kontentStoredAssets);
+    })
+
+    document.getElementById('filerobotImages').addEventListener('click', function(event) {
+        if (event.target.classList.contains('removeItem') || event.target.closest('.removeItem')) {
+            const parentItem =  event.target.closest('.removeItemParent');
+            const uuid = parentItem.getAttribute('data-uuid');
+            const filteredData = kontentStoredAssets.filter(item => item.file.uuid !== uuid);
+            kontentStoredAssets = filteredData;
+            CustomElement.setValue(JSON.stringify(filteredData));
+            countFiles -= 1
+            if (countFiles == 0) {
+                document.getElementById('filerobotOpenModalBtn').classList.remove('hidden')
+            }
+            parentItem.remove();
+        }
+    });
+
+});
+
+
+
+
